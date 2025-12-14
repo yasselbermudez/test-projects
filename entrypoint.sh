305ad1a2a93a7ac17a6a2b7201076c57c3e80aeb
yasselbermudez <yasselbermudez8@gmail.com>
2025-12-14 07:50:31 -0600

    dockerizing the app and change the db name

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#El script entrypoint.sh expande la variable $PORT 
+#correctamente antes de ejecutar uvicorn. 
+
+#!/bin/bash
+
+# Si la variable PORT no está definida, usa 8000 por defecto
+PORT=${PORT:-8000}
+
+# Ejecuta uvicorn
+exec uvicorn app.main:app --host 0.0.0.0 --port $PORT
